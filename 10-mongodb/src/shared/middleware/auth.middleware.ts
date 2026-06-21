294367e7e9a0b83cd4c353c0f195d457b07111d6
mustafatawab <engrmustafatawab05@gmail.com>
2026-06-21 18:24:48 +0500

    auth middleware

--- 10-mongodb/src/shared/middleware/auth.middleware.ts
+++ 10-mongodb/src/shared/middleware/auth.middleware.ts
@@ -4,7 +4,30 @@ import type { Request, Response, NextFunction } from "express";
 
 export const authMiddleware = async (req : Request, res: Response, next: NextFunction) => {
     try {
-        
+       const authHeader = req.headers.authorization;
+    
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      throw new AppError("You are not logged in. Please provide a token.", 401);
+    }
+
+    // 2. Extract the actual token from the string ("Bearer <TOKEN>")
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+      throw new AppError("Authentication token is missing.", 401);
+    }
+
+    // 3. Verify the token token using your environment secret
+    const jwtSecret = process.env.JWT_SECRET || "your_fallback_super_secret_key";
+    
+    const decoded = jwt.verify(token, jwtSecret) as Express.Request["user"];
+
+    // 4. Attach the decoded user payload to the request object
+    // This allows downstream Controllers to read `req.user.id` or `req.user.role`
+    req.user = decoded;
+
+    // 5. Let the request move forward to the next middleware or Controller
+    next(); 
     } catch (error) {
         next(error)
     }
